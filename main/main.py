b7666c12d5c8eaed3fcaf9b90afb28445a7a24ba
Fraank <fraank@live.de>
2024-03-10 13:01:39 +0100

    main.py mit beispielcode gefüllt

--- main/main.py
+++ main/main.py
@@ -1,1 +1,12 @@
-print("Hi")
+import paho.mqtt.client as cli
+import paho.mqtt.subscribe as sub
+auth = dict(username="mqtt-user",password="tryit1ce")
+
+msg = sub.simple("shellies/shellies/#", 
+                 hostname="192.168.178.25",
+                 port=1883,
+                 auth=auth
+                 )
+
+print(msg)
+print(msg.topic)
