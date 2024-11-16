4b45b1a61b74aa92edf591b88efa8dbf6586b0cf
Fraank-Dash <frank.fischer92@gmail.com>
2024-11-16 21:37:57 +0100

    the "main.py" is now up to date

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-#__CONFIG__
-topics="shellies/shellies/#"
-auth = dict(username="mqtt-user",password="tryit1ce")
-hostname="192.168.178.25"
-#__INIT__
-import paho.mqtt.subscribe as sub
-from paho.mqtt import __version__ as vsn
-import time
-from datetime import timedelta
-import datetime
-print(vsn)
-ref_time = time.monotonic(), datetime.datetime.now()
-print(ref_time)
+from source import broker2db
+# this should do the trick...
 
-#__FI..__ (First In)
-msg = sub.simple(topics=topics, #from __CONFIG__
-                 hostname=hostname, #from __CONFIG__
-                 port=1883,
-                 auth=auth, #from __CONFIG__
-                 msg_count=10)
-#__..FO__ (First Out)
-#or toilet-flush-out once in a while -> via numpy array func?
-dt0 = ref_time[0]
-for m in msg: # do something
-    dt = timedelta(seconds=m.timestamp-ref_time[0])
-    dt2 = timedelta(seconds=m.timestamp-dt0) #only makes sense if the topic is the same, else close to 0 -> see results
-    msg_time=ref_time[1]+dt
-    print("Ref_Time: ",ref_time[1],"\tMsg_Time: ",msg_time.time(),"\tdt_ref: ",dt, "\tdt_msg",dt2)
-    dt0 = m.timestamp
+try: 
+    x = broker2db.broker(config_file_name=r'config/config.yaml')
+except Exception as ex:
+    print(ex)
+try:
+    x.run()
+except Exception as ex:
+    print(ex)
