8f7c758dd581bc9eab8a92f4df4411769dfaa278
Fraank-Dash <frank.fischer92@gmail.com>
2024-11-16 21:26:33 +0100

    removed redundand code lines from broker2db.py, added some comments, fixed one bug in line 25 where the table name was still hard-coded instead of using the tablename of the config.yaml
    Wrote a little how to on the prequesites in the if __name__ = __main__ section, before i forget what i did.
    i also switched from postgres to the postgres extension timescale which i wanted to mention

--- source/broker2db.py
+++ source/broker2db.py
@@ -6,24 +6,40 @@ from paho.mqtt import client as mqtt_client
 import time
 from datetime import timedelta,datetime
 ##POSTGRES
-import psycopg2
-from sqlalchemy import create_engine, MetaData,VARCHAR,DATETIME
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import inspect,text,Table
-import pandas as pd
-import numpy 
 
 
 class broker:
     def __init__(self, config_file_name:str,msg_config:str='print'):
+        """
+        I am not happy with the class name 'broker' since it is more like a one-directional interface 
+        
+        I did not care about security like ssl, password security or anything else since this is more like a proof of concept! 
+        Therefore, i assume, that, if you can read and UNDERSTAND this code, it is obvious to you, that storing ip adresses, 
+        usernames and passwords within a single 'config.yaml' file is a stupid idea! 
+        (And uploading it as a public repository to the internet is worse!)
+        So, if you use this code, be aware of the risks. 
+        but what it basically does is: 
+        a) load a yaml.config based on the "config/config.yaml"-File 
+        b) the database connection is established based on the in a) loaded config file
+        c) an  mqtt-client is created which connects via the in a) mentioned broker-settings
+
+
+        """
+        #loading config
         self.conf = self.load_config_file(config_file_name)
-        #db
+        #db-connection
         self._engine = self.create_engine_from_conf(conf=self.conf)
-        self.db_connection = self._engine.connect()
+        try:
+            self.db_connection = self._engine.connect()
+        except ConnectionError as ex:
+            print(ex)
         self.metadata = MetaData()
         self.metadata.reflect(bind=self._engine)
-        self.pg_table = Table('pg_table', self.metadata, autoload = True)
-        #broker
+        self.pg_table = Table(self.conf['db_server']['database']['db_table'], self.metadata, autoload = True)
+        #broker-connection
         self.ref_time = self.get_reference_time()
         self.client = mqtt_client.Client(clean_session=True)
         try:
@@ -33,21 +49,23 @@ class broker:
             print(ex)
         self.client.connect(host=self.conf['mqtt_server']['ip'],
                             port=self.conf['mqtt_server']['port'])
+        ##subscriptions
         for topic in self.conf['mqtt_server']['topics']:
             self.client.subscribe(topic)
-
+        ##other whacky stuff
         self.client.on_connect = self.on_connect()
-
         self.client.on_message = self.on_message(msg_config)
 
     def load_config_file(self,file_name:str):
-        """
+        """func to load configfile
         """
         file = open(file=file_name,mode='r')
         conf = yaml.safe_load(file)
         return conf
     
     def on_connect(self):
+        """
+        """
         def on_connect(client,userdata,flags,rc):
             if rc == 0:
                 print("Connected to MQTT Broker!")
@@ -55,6 +73,11 @@ class broker:
                 print("Failed to connect, return code %d\n", rc)
         return on_connect
     def on_message(self,msg_config:str):
+        """3 different options for 'on message'
+        well, actually 2 different options...
+        i thought about writing into a csv-file or other stuff as well for testing, 
+        but writing to a database worked so well that i havent added this option
+        """
         if msg_config=='print':
             def on_message(client,userdata,msg):
                 _vals = *(self.t_mono2t_datetime(ref_time=self.ref_time,msg_timestamp=msg.timestamp)), msg.payload.decode(), msg.topic
@@ -76,7 +99,9 @@ class broker:
         return on_message
     
     def get_reference_time(self):
-        """"""
+        """
+        lorem ipsum
+        """
         return dict(t_mono=time.monotonic(), t_dt=datetime.now())
 
     def t_mono2t_datetime(self,ref_time:dict, msg_timestamp:int) -> dict:
@@ -86,6 +111,7 @@ class broker:
     ##POSTGRES-DATABASE
     def create_engine_from_conf(self,conf:dict):
         """
+        lorem ipsum
         """
         engine = create_engine('{db_language}+{db_adapter}://{user}:{pw}@{ip}:{port}/{db_name}'.
                                 format(db_language=conf['db_server']['database']['db_language'],
@@ -101,18 +127,56 @@ class broker:
 
     #run
     def run(self):
+        """lorem ipsum
+        the only way to stop is Ctrl+C or Alt+F4 :D
+        """
         self.client.loop_forever()
 
 
 if __name__ == '__main__':
-    
-    #terminal PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres
-    #postgres=#\c pg_database
-    #pg_database=# CREATE TABLE pg_table(
-    #msg_id BIGSERIAL PRIMARY KEY,
-    #msg_date TIMESTAMP WITH TIME ZONE,
-    #msg_topic varchar(100),
-    #msg_value varchar(10));
+    # STEPS TO DO iN ORDER TO SET EVERYTHING UP (but not yet an automated part of the setup
+    # ___Python_ENV___
+    #conda create --name mqtt2pg python=3.13
+    #conda activate mqtt2pg
+    #conda install pyyaml conda-forge::paho-mqtt psycopg2 sqlalchemy pandas numpy networkx
+
+    # ___DOCKER___
+    # 1. MQTT-Broker
+        # -> basic installation... https://hub.docker.com/_/eclipse-mosquitto/
+        # docker pull eclipse-mosquitto
+        # docker run -it -p  1883:1883 ...
+
+    # 2. Postgres (Timescale(!))
+        # -> basic installation... https://docs.timescale.com/self-hosted/latest/install/installation-docker/
+        # Timescale because handling timeseries with hypertables, pre written timeseries aggregation etc.
+        # docker pull timescale/timescaledb-ha:pg16
+        # docker run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres timescale/timescaledb-ha:pg16
+
+    # ___Postgres-Terminal or via PGAdmin4___
+    # 1. create new db
+        #CREATE DATABASE staging_mqtt
+        #    WITH
+        #    OWNER = postgres
+        #    ENCODING = 'UTF8'
+        #    LC_COLLATE = 'C.UTF-8'
+        #    LC_CTYPE = 'C.UTF-8'
+        #    LOCALE_PROVIDER = 'libc'
+        #    TABLESPACE = pg_default
+        #    CONNECTION LIMIT = -1
+        #    IS_TEMPLATE = False;
+        #
+        #COMMENT ON DATABASE staging_mqtt
+        #    IS 'Staging für den mqtt_client';
+
+    # 2. create table
+        # see https://docs.timescale.com/use-timescale/latest/hypertables/create/
+
+        # CREATE TABLE tbl_staging_mqtt (
+        #    msg_date   TIMESTAMPTZ NOT NULL,
+        #    msg_topic  TEXT        NOT NULL,
+        #    msg_value  TEXT        NOT NULL
+        #    );
+        # SELECT create_hypertable('tbl_staging_mqtt', by_range('msg_date'));
 
 
     file_name=r'config/config.yaml'
